79fad297efc27ef703fec1cacb68f61ed0f2cce0
NatinaelFekadu <101578729+NatinaelFekadu@users.noreply.github.com>
2023-02-11 01:27:19 -0800

    Create sort colors.py

---
+++ sort colors.py
@@ -0,0 +1,11 @@
+import math
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # new_arr=[]
+        for i in range(len(nums)):
+            for j in range(len(nums)-1):
+                if nums[j] > nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
