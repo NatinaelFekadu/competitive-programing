9564d5d352098bcde14bb4fe4b656e3dde8f601b
Natinael Fekadu <101578729+NatinaelFekadu@users.noreply.github.com>
2024-02-08 03:33:45 -0800

    Add solution for Corporate Flight Bookings

---
+++ dsa/leetcode/corporate-flight-bookings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        arr=[0]*(n+1)
+        for i in bookings:
+            arr[i[0]-1]+=i[2]
+            arr[i[1]]-=i[2]
+        s=0
+        for i in range(n):
+            s+=arr[i]
+            arr[i]=s
+        return arr[:n]
